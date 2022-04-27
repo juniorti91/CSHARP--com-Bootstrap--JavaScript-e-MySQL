13037d4d6e582ca8cce87a9d3bab854d2d14c48c
juniorti91 <juniortidesenvolvimento1@gmail.com>
2022-04-26 23:15:52 -0300

    Crud CSharp, com Bootestrap, JavaScript e Mysql

---
+++ ControleDeContatos/ControleDeContatos/Program.cs
@@ -0,0 +1,32 @@
+using ControleDeContatos.Data;
+using ControleDeContatos.Repositorio;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DataBase");
+builder.Services.AddDbContext<BancoContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddScoped<IContatoRepositorio, ContatoRepositorio>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
